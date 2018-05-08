861d7af17303d39a330738b293d20bfd5073bd14
caijie <caijie@think-land.com>
2018-05-08 17:18:17 +0800

    add 0(nlogn) sort algorithm and binary search

--- sort_algorithm.py
+++ sort_algorithm.py
@@ -14,6 +14,19 @@ def main():
     bubble_sort(lyst)
     insertion_sort(lyst)
 
+    # classic O(nlogn) sort algorithm
+    quick_sort(lyst)
+    merge_sort(lyst)
+
+    # fibnacci queue
+    print(fib(20))
+
+    # binary search
+    lyst.sort()
+    binary_search(2, lyst)
+
+
+
 
 def swap(lyst, i, j):
     tmp = lyst[i]
@@ -60,6 +73,97 @@ def insertion_sort(lyst):
     print("insertion sorted list: %s" % lyst)
 
 
+def quick_sort(lyst):
+    quick_sort_helper(lyst, 0, len(lyst)-1)
+    print("quicksort sored list: %s" % lyst)
+
+
+def quick_sort_helper(lyst, left, right):
+    if left < right:
+        pivot_index = partition(lyst, left, right)
+        quick_sort_helper(lyst, left, pivot_index-1)
+        quick_sort_helper(lyst, pivot_index+1, right)
+
+
+def partition(lyst, left, right):
+    middle = (left+right)//2
+    pivot = lyst[middle]
+    lyst[middle] = lyst[right]
+    lyst[right] = pivot
+
+    boundary = left
+    for i in range(left, right):
+        if lyst[i] < pivot:
+            swap(lyst, boundary, i)
+            boundary += 1
+    swap(lyst, boundary, right)
+    return boundary
+
+
+def merge_sort(lyst):
+    buffer = [None]*len(lyst)
+    merge_sort_helper(lyst, 0, len(lyst)-1, buffer)
+    for i in range(len(buffer)):
+        lyst[i] = buffer[i]
+    print("mergesort sorted list: %s" % lyst)
+
+
+def merge_sort_helper(lyst, left, right, buffer):
+    if left < right:
+        middle = (left + right)//2
+        merge_sort_helper(lyst, left, middle, buffer)
+        merge_sort_helper(lyst, middle+1, right, buffer)
+        merge(lyst, left, middle, right, buffer)
+
+
+def merge(lyst, left, middle, right, buffer):
+    i1 = left
+    i2 = middle+1
+    for i in range(left, right+1):
+        if i1 > middle:
+            buffer[i] = lyst[i2]
+            i2 += 1
+        elif i2 > right:
+            buffer[i] = lyst[i1]
+            i1 += 1
+        elif lyst[i1] < lyst[i2]:
+            buffer[i] = lyst[i1]
+            i1 += 1
+        else:
+            buffer[i] = lyst[i2]
+            i2 += 1
+
+
+def fib(n):
+    if n < 3:
+        return 1
+    else:
+        a1 = 1
+        a2 = 1
+        count = 3
+        while count <= n:
+            sum = a1 + a2
+            a1 = a2
+            a2 = sum
+            count += 1
+        return sum
+
+
+def binary_search(target, lyst):
+    left = 0
+    right = len(lyst) - 1
+    while left <= right:
+        middle = (left+right)//2
+        print(middle, lyst[middle])
+        if target == lyst[middle]:
+            print("got it , target's index is %s" % middle)
+            return middle
+        elif target < lyst[middle]:
+            right = middle-1
+        else:
+            left = middle + 1
+
+    print("cannot find the target return -1")
 
 
 
